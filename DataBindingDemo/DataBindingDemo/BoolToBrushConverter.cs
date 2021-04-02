7861d1e9a506547913318b67181486cf1dc2a1b1
maidinhthanhtung <80858835+maidinhthanhtung@users.noreply.github.com>
2021-04-02 18:26:19 +0300

    WPF3

---
+++ DataBindingDemo/DataBindingDemo/BoolToBrushConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Globalization;
+
+namespace DataBindingDemo
+{
+    [ValueConversion(typeof(bool), typeof(Brush))]
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(
+               object value,
+               Type targetType,
+               object parameter,
+               CultureInfo culture)
+        {
+            Brush b = null;
+            if (value != null &&
+            value.GetType() == typeof(bool))
+            {
+                b = (bool)value ? Brushes.Green : Brushes.Red;
+            }
+            return b;
+        }
+        public object ConvertBack(
+               object value,
+               Type targetType,
+               object parameter,
+               CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
